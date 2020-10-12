b1a68b5836fb97f4c4b2ade7019db510ac7fc65d
sa1amander83 <71230606+sa1amander83@users.noreply.github.com>
2020-10-12 15:19:21 +0300

    домашка 11

--- zad4.js
+++ zad4.js
@@ -9,15 +9,3 @@ function myFunc(a, b) {
    }, 1000);
  }
  myFunc(5, 15);  
-=======
-function myFunc(a, b) {
-   let timerId = setInterval(function() {
-     console.log(a);
-     if (a == b) {
-      clearInterval(timerId);
-     }
-     a++;
-   }, 1000);
- }
- myFunc(5, 15);
->>>>>>> d5306730856535c7f2dedef04ea55f7078a06ddf
