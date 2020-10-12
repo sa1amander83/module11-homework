f3953e738b782c7a7f30b3cc1e3f6e25538f6e1c
sa1amander83 <71230606+sa1amander83@users.noreply.github.com>
2020-10-12 15:18:55 +0300

    домашка 11

--- zad3.js
+++ zad3.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-
-// function myFunc1(a) {
+//function myFunc1(a) {
 //     function myFunc2(b) {
 //     console.log(a + b);
 //     }
@@ -9,28 +7,11 @@
 // myFunc1(20); 
 
 //или  так 
+
 function myFunc1(a) {
 	return function myFunc2(b) {
   	  return a + b;
   };
 }  
 const summ = myFunc1(10);
-=======
-
-// function myFunc1(a) {
-//     function myFunc2(b) {
-//     console.log(a + b);
-//     }
-//   myFunc2(10);
-// }
-// myFunc1(20); 
-
-//или  так 
-function myFunc1(a) {
-	return function myFunc2(b) {
-  	return a + b;
-  };
-}
-const summ = myFunc1(10);
->>>>>>> d5306730856535c7f2dedef04ea55f7078a06ddf
-console.log(summ(7)); 
+console.log(summ(20));
